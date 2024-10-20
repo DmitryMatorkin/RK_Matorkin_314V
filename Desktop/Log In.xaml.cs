1efba34320c7aa93197ec125d06b6713cf7b2fa4
DmitryMatorkin <matorkin002@gmail.com>
2024-10-20 20:53:50 +0300

    реализованы окна и placeholder

--- Desktop/Log In.xaml.cs
+++ Desktop/Log In.xaml.cs
@@ -23,5 +23,70 @@ namespace Desktop
         {
             InitializeComponent();
         }
+        private void EmailTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            // Убираем текст placeholder при фокусе
+            if (EmailTextBox.Text == "Введите почту")
+            {
+                EmailTextBox.Text = "";
+            }
+
+            // Делаем вводимый текст черным
+            EmailTextBox.Foreground = Brushes.Black;
+            EmailTextBox.Opacity = 1.0;
+        }
+
+        private void EmailTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            // Возвращаем текст placeholder при потере фокуса, если поле пустое
+            if (string.IsNullOrWhiteSpace(EmailTextBox.Text))
+            {
+                EmailTextBox.Text = "Введите почту";
+                EmailTextBox.Foreground = Brushes.Gray;
+
+            }
+        }
+        private void TextBoxWithPlaceholder_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+            // Убираем текст placeholder при фокусе
+            if (TextBoxWithPlaceholder.Text == "Введите пароль")
+            {
+                TextBoxWithPlaceholder.Text = "";
+            }
+
+            // Делаем вводимый текст черным
+            TextBoxWithPlaceholder.Foreground = Brushes.Black;
+            TextBoxWithPlaceholder.Opacity = 1.0;
+        }
+
+        private void TextBoxWithPlaceholder_LostFocus(object sender, RoutedEventArgs e)
+        {
+            // Возвращаем текст placeholder при потере фокуса, если поле пустое
+            if (string.IsNullOrWhiteSpace(TextBoxWithPlaceholder.Text))
+            {
+                TextBoxWithPlaceholder.Text = "Введите пароль";
+                TextBoxWithPlaceholder.Foreground = Brushes.Gray;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        public static class WindowManager
+        {
+
+            public static void SwitchWindow(Window currentWindow, Window newWindow)
+            {
+                newWindow.Show();
+                currentWindow.Hide();
+            }
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Reg reg = new Reg();
+            WindowManager.SwitchWindow(this, reg);
+        }
     }
 }
