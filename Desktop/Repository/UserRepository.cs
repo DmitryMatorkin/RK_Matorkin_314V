67d48ceb4411461429d043c4044363992e1fd638
DmitryMatorkin <matorkin002@gmail.com>
2024-10-21 18:08:25 +0300

    .

---
+++ Desktop/Repository/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Todo.Entities;
+
+namespace Desktop.Repository
+{
+   internal class UserRepository
+    {
+        private List<UserModel> _users = new List<UserModel>()
+        {
+        new UserModel {Name = "Lukoshko", Email = "", Password = ""}
+        };
+    }
+}
--- Todo.Entities/Class1.cs
+++ Todo.Entities/Class1.cs
@@ -3,7 +3,17 @@ namespace Todo.Entities
 {
     public class UserModel
     {
-
+        public UserModel? GetUser(string email,  string password)
+        {
+            foreach (var user in _users)
+            {
+                if (user.Email == email && user.Password = password) 
+                {
+                return user;
+                }
+            }
+            return null;
+        }
     }
 
 }
